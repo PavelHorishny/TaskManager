f2be15fb25dd8736e259baedd452947da890d79d
Pavel Horishny <horishnypavel@gmail.com>
2022-11-27 21:33:55 +0200

    refactored Main.class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,34 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException, SQLException {
-/*        Task t = new Task();
-        t.setName("terr");
-        t.setSTime(5);
-        t.setCreatedDateTime("lll");
-        Task t1 = new Task();
-        t1.setName("terr1");
-        t1.setSTime(51);
-        t1.setCreatedDateTime("lll1");
-        DataService dataService = new DataService();
-        dataService.resolvePost("newTest1");*/
-/*        Task t = new Task();
-        t.setName("terr");
-        t.setSTime(5);
-        t.setCreatedDateTime("lll");*/
-
-/*        TaskController tc = new TaskController();
-        Response r = tc.put(3,null,"yes",5);
-        System.out.println(r.toString());
-        HibernateUtil.shutdown();*/
-
-/*        try {
-          ts.getByID(3);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        HibernateUtil.shutdown();*/
+    public static void main(String[] args) {
         SwingUtilities.invokeLater(new TaskGui()::createAndShow);
     }
 }
