08627c0b7af72acecab99f96d8dffd342a80b74e
Pavel Horishny <horishnypavel@gmail.com>
2022-12-01 01:32:47 +0200

    task Panel now looks ok

--- src/main/java/com/taskManager/gui/TaskPanel.java
+++ src/main/java/com/taskManager/gui/TaskPanel.java
@@ -13,113 +13,140 @@ import java.util.Date;
 public class TaskPanel extends JPanel {
     SimpleDateFormat df = new SimpleDateFormat("dd MMM yyyy");
     TaskController taskController = new TaskController();
+    GridBagConstraints gbc;
 
     public TaskPanel(Task task, MainPanel main, TopPanel topPanel){
         //TODO refactor and make panel to look nice
         this.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
         this.setLayout(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.fill = GridBagConstraints.RELATIVE;
+
         JLabel idLabel = new JLabel("id ");
         idLabel.setOpaque(true);
         idLabel.setBackground(Color.GRAY);
-        idLabel.setPreferredSize(new Dimension(50,20));
+        gbc = new GridBagConstraints();
+        gbc.weightx = 1;
+        gbc.weighty=0;
         gbc.gridx = 0;
         gbc.gridy = 0;
-        gbc.anchor = GridBagConstraints.LINE_START;
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.insets = new Insets(2,5,2,2);
+/*
+        gbc.ipadx =340;
+        gbc.ipady = 100;
+        gbc.anchor = GridBagConstraints.EAST;*/
         this.add(idLabel,gbc);
+
         JLabel idText = new JLabel(String.valueOf(task.getId()));
+        idText.setOpaque(true);
         idText.setBackground(Color.YELLOW);
         gbc = new GridBagConstraints();
         gbc.gridx = 1;
         gbc.gridy = 0;
-        gbc.anchor = GridBagConstraints.LINE_END;
+        gbc.weightx = 1;
+        gbc.insets = new Insets(2,2,2,2);
+        gbc.anchor = GridBagConstraints.WEST;
+/*
+        gbc.ipadx =340;
+        gbc.ipady = 100;*/
         this.add(idText,gbc);
+
         JLabel nameLabel = new JLabel("Task name ");
-        idLabel.setOpaque(true);
-        idLabel.setBackground(Color.GRAY);
-        idLabel.setPreferredSize(new Dimension(50,20));
+        nameLabel.setOpaque(true);
+        nameLabel.setBackground(Color.GREEN);
+        gbc = new GridBagConstraints();
         gbc.gridx = 0;
         gbc.gridy = 1;
-        gbc.anchor = GridBagConstraints.LINE_START;
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.insets = new Insets(2,5,2,2);
         this.add(nameLabel,gbc);
+
         JLabel nameText = new JLabel(task.getName());
-        idText.setBackground(Color.YELLOW);
         gbc = new GridBagConstraints();
         gbc.gridx = 1;
         gbc.gridy = 1;
-        gbc.anchor = GridBagConstraints.LINE_END;
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.insets = new Insets(2,2,2,2);
         this.add(nameText,gbc);
+
         JLabel startDateLabel = new JLabel("Start date ");
-        idLabel.setOpaque(true);
-        idLabel.setBackground(Color.GRAY);
-        idLabel.setPreferredSize(new Dimension(50,20));
+        gbc = new GridBagConstraints();
         gbc.gridx = 0;
         gbc.gridy = 2;
-        gbc.anchor = GridBagConstraints.LINE_START;
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.insets = new Insets(2,5,2,2);
         this.add(startDateLabel,gbc);
+
         JLabel startDateText = new JLabel(df.format(task.getCreatedDateTime()));
-        idText.setBackground(Color.YELLOW);
         gbc = new GridBagConstraints();
         gbc.gridx = 1;
         gbc.gridy = 2;
-        gbc.anchor = GridBagConstraints.LINE_END;
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.insets = new Insets(2,2,2,2);
         this.add(startDateText,gbc);
+
         JLabel endDateLabel = new JLabel("End date ");
-        idLabel.setOpaque(true);
-        idLabel.setBackground(Color.GRAY);
-        idLabel.setPreferredSize(new Dimension(50,20));
         gbc.gridx = 0;
         gbc.gridy = 3;
-        gbc.anchor = GridBagConstraints.LINE_START;
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.insets = new Insets(2,5,2,2);
         this.add(endDateLabel,gbc);
+
+        //TODO make separate method to check date
         JLabel endDateText;
         if(task.getFinishedDateTime()!=null) {
             endDateText = new JLabel(df.format(task.getFinishedDateTime()));
         }else {
             endDateText = new JLabel("not finished yet");
         }
-        idText.setBackground(Color.YELLOW);
         gbc = new GridBagConstraints();
         gbc.gridx = 1;
         gbc.gridy = 3;
-        gbc.anchor = GridBagConstraints.LINE_END;
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.insets = new Insets(2,2,2,2);
         this.add(endDateText,gbc);
+
         JLabel timeLabel = new JLabel("Spent time ");
-        idLabel.setOpaque(true);
-        idLabel.setBackground(Color.GRAY);
-        idLabel.setPreferredSize(new Dimension(50,20));
         gbc.gridx = 0;
         gbc.gridy = 4;
-        gbc.anchor = GridBagConstraints.LINE_START;
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.insets = new Insets(2,5,2,2);
         this.add(timeLabel,gbc);
+
         JLabel timeText = new JLabel(TestingTimer.convertTime(task.getSTime()));
-        idText.setBackground(Color.YELLOW);
         gbc = new GridBagConstraints();
         gbc.gridx = 1;
         gbc.gridy = 4;
-        gbc.anchor = GridBagConstraints.LINE_END;
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.insets = new Insets(2,2,2,2);
         this.add(timeText,gbc);
+
         JLabel descriptionLabel = new JLabel("Short description ");
-        idLabel.setOpaque(true);
-        idLabel.setBackground(Color.GRAY);
-        idLabel.setPreferredSize(new Dimension(50,20));
         gbc.gridx = 0;
         gbc.gridy = 5;
-        gbc.anchor = GridBagConstraints.LINE_START;
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.insets = new Insets(0,5,190,0);
         this.add(descriptionLabel,gbc);
+
         JLabel descriptionText = new JLabel("nothing here yet");
         idText.setBackground(Color.YELLOW);
         gbc = new GridBagConstraints();
         gbc.gridx = 1;
         gbc.gridy = 5;
-        gbc.anchor = GridBagConstraints.LINE_END;
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.insets = new Insets(0,0,190,0);
         this.add(descriptionText,gbc);
+
         JButton finish = new JButton("Finish task");
+        gbc = new GridBagConstraints();
         finish.addActionListener(e->{
             taskController.put(task.getId(),null,"finished", task.getSTime());
             //TODO resolve response
         });
-        this.add(finish);
+        gbc.gridx = 1;
+        gbc.gridy = 6;
+        gbc.anchor = GridBagConstraints.EAST;
+        gbc.insets = new Insets(0,0,0,5);
+        this.add(finish, gbc);
+
     }
 }
